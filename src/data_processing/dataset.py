6197b020e9bb5322657a98fc8749b79e0467f84e
zakaria narjis <zakaria.narjis.97@gmail.com>
2024-12-09 21:30:43 +0100

    update dataset

--- src/data_processing/dataset.py
+++ src/data_processing/dataset.py
@@ -7,9 +7,10 @@ from src.data_processing.labeler import Labeler
 import numpy as np
 import torch
 from tqdm import tqdm
+import os
 
 class iScatDataset(Dataset):
-    def __init__(self, image_paths, target_paths, seg_args=None,image_size=(224,224),train=True,preload_image=False):
+    def __init__(self, image_paths, target_paths, seg_args=None,image_size=(224,224),train=True,preload_image=False,reload_mask=False):
         self.image_paths = image_paths
         self.target_paths = target_paths #list of tuple of paths
         self.seg_args = seg_args
@@ -17,14 +18,20 @@ class iScatDataset(Dataset):
         self.preload_image = preload_image
         self.seg_args = seg_args
         if self.preload_image:
-            self.images = np.concatenate([nd2.imread(image_path)[0] for image_path in self.image_paths])
+            self.images = []
+            for image_path in tqdm(self.image_paths,desc="Loading surface images to Memory"):
+                self.images.append(nd2.imread(image_path)[0])           
+            self.images = np.concatenate([self.images],axis=0)
+            self.images = np.repeat(self.images,100,axis=0)
             self.images = torch.from_numpy(self.images)
         else:
-            self.image_paths = self.image_paths*100
+            self.image_paths = self.image_paths
             self.target_paths = self.target_paths
-            self.image_paths = np.concatenate(self.image_paths)
-        self.labeler = Labeler()
+            self.image_paths = np.concatenate([self.image_paths])
+            self.image_paths = np.repeat(self.image_paths,100,axis=0)
 
+        if reload_mask or not all([os.path.exists(os.path.join(os.path.dirname(target_path[0]),"mask.npy"))for target_path in self.target_paths]):
+            self.labeler = Labeler()
         #default segmentation arguments
         if self.seg_args is None:
             args={
@@ -40,7 +47,11 @@ class iScatDataset(Dataset):
             total=len(self.target_paths), 
             desc="Creating Masks"      
         ):
-            mask = self.labeler.label(fluorescence_images_paths, seg_args, segmentation_method="comdet")
+            if reload_mask==False and os.path.exists(os.path.join(os.path.dirname(fluorescence_images_paths[0]),"mask.npy")):
+                mask = np.load(os.path.join(os.path.dirname(fluorescence_images_paths[0]),"mask.npy"))
+            else:                   
+                mask = self.labeler.label(fluorescence_images_paths, seg_args, segmentation_method="comdet")
+                np.save(os.path.join(os.path.dirname(fluorescence_images_paths[0]),"mask.npy"),mask)
             self.masks.append(mask)
         self.masks = np.concatenate([self.masks],axis=0)
         self.masks = np.repeat(self.masks,100,axis=0)
